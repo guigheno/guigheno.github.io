2ebcc08cf61092527f510d7b64eee3bfed118a7d
Guilherme Gheno <guigheno@gmail.com>
2026-08-17 22:00:32 -0300

    fix: site URL para guigheno.github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://daisyeng.github.io',
+  site: 'https://guigheno.github.io',
   output: 'static',
 });
